f3b706e27b212fd28713050d0a92b48054d6c5a1
iliyasBr <iliyasboumour@gmail.com>
2022-02-21 03:06:18 +0100

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { DatabaseModule } from './database/database.module';
         POSTGRES_HOST: Joi.string().required(),
         POSTGRES_PORT: Joi.number().required(),
         POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
+        // POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
         PORT: Joi.number(),
       }),
--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,5 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Post {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   title: string;
+  @Column('text')
   content: string;
 }
