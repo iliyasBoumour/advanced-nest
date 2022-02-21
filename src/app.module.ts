1f65020f37c97ed1ea9f30000d289396f8f2a543
iliyasBr <iliyasboumour@gmail.com>
2022-02-21 02:34:32 +0100

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 // to validate our env variables
 import * as Joi from '@hapi/joi';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -19,8 +18,9 @@ import { PostsModule } from './posts/posts.module';
         PORT: Joi.number(),
       }),
     }),
+    DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
