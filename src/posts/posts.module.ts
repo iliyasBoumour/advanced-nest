83f650353a8c7610d086861aa1431fc3f6d9e354
iliyasBr <iliyasboumour@gmail.com>
2022-04-12 04:41:40 +0200

    in memory cache clearing cache

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forFeature([Post]),
     // time that a response is cached before deleting it is 5 seconds
     //  the maximum number of elements in the cache is 100
-    CacheModule.register({ ttl: 5, max: 100 }),
+    CacheModule.register({ ttl: 120, max: 100 }),
   ],
   controllers: [PostsController],
   providers: [PostsService],
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Like, Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
@@ -11,13 +16,32 @@ export class PostsService {
   constructor(
     @InjectRepository(Post)
     private readonly postsRepository: Repository<Post>,
+    //  we can interact with the cache manually. First we need to inject it into our service.
+    //  the cache manager provides a key-value store
+    //  cacheManager.get('key') ,
+    // cacheManager.set('key', 'value),
+    // cacheManager.del('key'),
+    // clear the whole cache: cacheManager.reset()
+    @Inject(CACHE_MANAGER) private cacheManager,
   ) {}
-  create(createPostDto: CreatePostDto, user: User): Promise<Post> {
+
+  // we invalidate our cache when the list of posts should change. With that, we can increase the Time To Live (TTL) and increase our application’s performance.
+  private async clearCache() {
+    const keys: string[] = await this.cacheManager.store.keys();
+
+    keys.forEach((key) => {
+      if (key.startsWith('/posts')) {
+        this.cacheManager.del(key);
+      }
+    });
+  }
+
+  async create(createPostDto: CreatePostDto, user: User): Promise<Post> {
     const newPost: Post = this.postsRepository.create({
       ...createPostDto,
       author: user,
     }); // const newPost=new Post(args)
-
+    await this.clearCache();
     return this.postsRepository.save(newPost);
   }
 
@@ -45,11 +69,13 @@ export class PostsService {
   async update(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
     const post: Post = await this.findOne(id);
     const newPost: Post = { ...post, ...updatePostDto };
+    await this.clearCache();
     return this.postsRepository.save(newPost);
   }
 
   async remove(id: number): Promise<void> {
     const post = await this.findOne(id);
+    await this.clearCache();
     await this.postsRepository.remove(post);
   }
 }
